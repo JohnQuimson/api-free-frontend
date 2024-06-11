fb063ee1c9be0cab3ae9106ab0d13aed9e7aee4b
JohnQuimson <john.quimson777@gmail.com>
2024-06-11 10:13:53 +0200

    feat: install axios

--- src/store.js
+++ src/store.js
@@ -1,6 +1,16 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  searchText: '',
-  loading: true,
+  api: {
+    baseUrl: 'http://localhost:3000',
+    apiUrls: {
+      posts: '/posts',
+      categories: '/categories',
+      tags: '/tags',
+    },
+  },
+  posts: [],
+  categories: [],
+  tags: [],
 });
+export default store;
